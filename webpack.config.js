5ac52430ac7d45ef1499281425c13075ed0dc350
codey4k <codey4k@gmail.com>
2017-01-08 22:34:40 +0000

    Start of the project

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: './build',
+        filename: 'bundle.js'
+    },
+    
+    resolve: {
+        extensions: ['', '.js']
+    },
+    
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        inline: true
+    },
+    
+    module: {
+        loaders: [
+            {
+                test: /(\.js|.jsx)$/,
+                loader: 'babel',
+                exclude: '/node_modules/',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+}
