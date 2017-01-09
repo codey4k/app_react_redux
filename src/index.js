1dbf9d1f9880f2480970583866eefdf1e6d46833
codey4k <codey4k@gmail.com>
2017-01-09 00:00:11 +0000

    Adding react react-dom babel-preset-react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo");
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    
+  render() {
+      return ( <h1>Hola mundo</h1>)
+  }
+  
+}
+
+ReactDOM.render(<App />, document.body)
